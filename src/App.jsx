d0050f920a05d26a9bfba84037a094d3812f9681
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-11 20:12:06 +0530

    Providing store to the React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+
+import { Provider } from "react-redux";
+import store from "./redux/cake/store";
+
 import CakeContainer from "./components/CakeContainer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <CakeContainer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContainer />
+      </div>
+    </Provider>
   );
 }
 
