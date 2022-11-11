2e043a9336105db2c1deca821309709e0305c7bc
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-11 20:25:26 +0530

    connect and map State/dispatch with props

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const CakeContainer = () => {
+import { connect } from "react-redux";
+import { buyCake } from "../redux";
+
+const CakeContainer = (props) => {
   return (
     <>
-      <h2>No. of Cakes</h2>
-      <button>Buy Cake</button>
+      <h2>No. of Cakes - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </>
   );
 };
 
-export default CakeContainer;
+const mapStateToProps = (state) => {
+  return {
+    numOfCake: state.numOfCake,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
