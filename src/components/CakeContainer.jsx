7ad4c928fa6eea35b45fd688c3f26f5a37016da5
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-11 20:03:21 +0530

    Added actions

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <>
+      <h2>No. of Cakes</h2>
+      <button>Buy Cake</button>
+    </>
+  );
+};
+
+export default CakeContainer;
