8f78538b9edeb5adea292b2e6445f648b80803d2
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-11 11:32:37 +0530

    redux app with the 3 concepts - store, action, reducer

---
+++ index.js
@@ -0,0 +1,55 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+// Defining Actions types
+const BUY_CAKE = "BUY_CAKE";
+
+// The Action Creator - The Customer who buys a cake
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "Customer comes to buy a cake",
+  };
+}
+
+// Defining Initial State - The rack of cakes before the customer comes
+const initialState = {
+  numOfCakes: 10,
+};
+
+// Defining Reducer - The ShopKeeper who reduce the number of items from the rack
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // The Action in action - Customer buying the cake
+    case BUY_CAKE:
+      return {
+        ...state, // Make a copy of initial state - since we are not mutating the original state, instead we are copying it and mutate that copy
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// Creating Store - The shop
+const store = createStore(reducer);
+
+// The rack of cakes before the customer comes
+console.log("Initial State --> ", store.getState());
+
+// Store is subscribing the rack of cakes
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated State --> ", store.getState())
+);
+
+// Customer buys 1 cake
+store.dispatch(buyCake());
+
+// Customer buys 1 cake
+store.dispatch(buyCake());
+
+// Customer buys 1 cake
+store.dispatch(buyCake());
+
+// Store is unsubscribing the rack of cakes
+unsubscribe();
