08df4b46a0ab1792746fc0bf301364085d0b3e76
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-12 18:18:17 +0530

    default parameter syntax

--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -1,5 +1,6 @@
 import { BUY_CAKE } from "./cakeTypes";
 
+// 1 is a default parameter value, if no value is passed in, 1 is used -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters
 export const buyCake = (number = 1) => {
   return {
     type: BUY_CAKE,
