688423873aae69d3b812d57986a535cc3c75da65
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-12 17:12:33 +0530

    added redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,11 @@ import rootReducer from "./rootReducer";
 
 import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
